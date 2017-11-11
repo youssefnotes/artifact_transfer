3791df9d0a9301433cb70579f8af7dc85187df3a
mohamed.youssef <mohamed.youssef@agile-architecture.com>
2017-11-11 18:56:08 +0400

    cleanup start.sh for bauhaus.de

--- artprovenance/newOrg/bauhaus.de/start.sh
+++ artprovenance/newOrg/bauhaus.de/start.sh
@@ -7,13 +7,6 @@ function main {
 		docker-compose --project-name art -f docker-compose-provenance.yaml start
 		# sleep 10
 		
-		# docker exec cli0.egyptianmuseum.org bash -c 'cd channels && peer channel create -c mainchannel -f MainChannel.tx -o orderer.art.ifar.org:7050'
-		
-		# docker exec cli0.egyptianmuseum.org bash -c 'cd channels && peer channel join -b mainchannel.block'
-		# docker exec cli0.egyptianmuseum.org bash -c 'cd channels && peer channel update -o orderer.art.ifar.org:7050 -c mainchannel -f EGArtMSPanchors.tx'
-		# docker exec cli0.louvre.fr bash -c 'cd channels && peer channel join -b mainchannel.block'
-		# docker exec cli0.louvre.fr bash -c 'cd channels && peer channel update -o orderer.art.ifar.org:7050 -c mainchannel -f FRArtMSPanchors.tx'
-
 }
 
 main 
